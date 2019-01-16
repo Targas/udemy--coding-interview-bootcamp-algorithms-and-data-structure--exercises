d32b43a9725b81b22ff10e53f7ce535d98c08348
Targas <barbarossa@pr8.com.br>
2019-01-16 18:15:46 -0200

    terminado, mas ainda faltam alguns exercicios pra fazer

--- AlgoCasts-master/exercises/fib/index.js
+++ AlgoCasts-master/exercises/fib/index.js
@@ -8,6 +8,41 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoise(fn) {
+    const cache =  {};
+
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+    };
+
+    return result()
+}
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoise(slowFib);
+
+// function fib(n) {
+//     const result = [0, 1];
+//
+//     for (let i = 2; i <= m; i++;) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+//
+//         result.push(a + b);
+//     }
+//
+//     return result[n];
+// }
 
 module.exports = fib;
